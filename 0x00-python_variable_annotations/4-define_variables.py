891ab0988c71e7eac6d9e9b74570e0f6c2214b66
Paul Okoli <paulokoli7@gmail.com>
2023-04-27 16:08:35 +0000

    fix: 4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
+""" 4. Define variables
 """
-4. Define variables
-"""
-
 
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
-school: str =  "Holberton"
+school: str = "Holberton"
